056501f723af8e3e20ac8cb0f6ff9a24c13d91ad
gustavsavs <144766546+gustavsavs@users.noreply.github.com>
2024-04-23 08:40:36 +0300

    Add files via upload
    pievienoju nepieciesamos failus

---
+++ script.js
@@ -0,0 +1,13 @@
+const poga = document.querySelector('.js-poga')
+
+let punkti = 10;
+
+poga.addEventListener('click', () => {
+    punkti = punkti - 1
+
+    if(punkti > 0){
+poga.textContent = `Nospied mani vēl ${punkti}x`
+ } else {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+ }
+})
